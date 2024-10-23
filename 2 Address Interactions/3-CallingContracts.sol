8f1408054ea413d1d8f492406c0e8358cd510bd7
salvador <salvador.martinez.ksmg@gmail.com>
2024-10-23 14:31:28 -0600

    2.3 Call function

--- 2 Address Interactions/3-CallingContracts.sol
+++ 2 Address Interactions/3-CallingContracts.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+/* 
+1: Call Function
+
+Your Goal: Alert Hero
+Use the IHero interface and the hero address passed into sendAlert to alert the hero from the Sidekick contract.
+ */
+
+ interface IHero {
+    function alert() external;
+}
+
+contract Sidekick {
+    function sendAlert(address hero) external {
+        // TODO: alert the hero using the IHero interface
+        IHero(hero).alert();
+    }
+}
