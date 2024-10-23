18032abdb98466a59c9a51a577a9561ddf2abdd6
salvador <salvador.martinez.ksmg@gmail.com>
2024-10-23 12:16:11 -0600

    1 Unsigned Integers

--- 1 Solidity Introduction/1-SmartContracts.sol
+++ 1 Solidity Introduction/1-SmartContracts.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 /*
 1: Booleans
@@ -9,9 +10,26 @@ Create two public boolean storage variables on the contract: a and b.
 Store true in the variable a and false in the value b.
 */
 
-pragma solidity ^0.8.20;
-
 contract Contract {
     bool public a = true;
     bool public b = false;
+}
+
+/*
+2: Unsigned Integers
+
+Your Goal: Create Unsigned Integers!
+Let's create three public storage unsigned integers in our Contract: a, b, and sum.
+
+Define the variable a as an uint8 with an initial value between 0 and 255.
+
+Define the variable b as an uint16 with a value of at least 256.
+
+The variable sum should be a uint256 with the sum of the values stored in a and b.
+ */
+
+ contract Contract2 {
+    uint8 public a = 2;
+    uint16 public b = 256;
+    uint256 public sum = a + b;
 }
