8f1f5fb5b948f6766dd27ad47aa54b46d9db60d9
salvador <salvador.martinez.ksmg@gmail.com>
2024-10-23 12:14:22 -0600

    1 booleans

---
+++ 1 Solidity Introduction/1-SmartContracts.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+/*
+1: Booleans
+
+Your Goal: Add two boolean variables
+Create two public boolean storage variables on the contract: a and b.
+
+Store true in the variable a and false in the value b.
+*/
+
+pragma solidity ^0.8.20;
+
+contract Contract {
+    bool public a = true;
+    bool public b = false;
+}
