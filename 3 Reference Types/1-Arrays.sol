bdec1ed0a66c6cd07fe5ea536d5290db51c5c9dd
salvador <salvador.martinez.ksmg@gmail.com>
2024-11-25 15:03:18 -0600

    array fixed sum

--- 3 Reference Types/1-Arrays.sol
+++ 3 Reference Types/1-Arrays.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
+/* 
+1: Fixed Sum
+
+Your Goal: Find the Sum
+Create a pure, external function sum which takes a fixed size array of five unsigned integers.
+Find the sum of the unsigned integers and return it as a uint.
+ */
+
+ contract Contract {
+    function sum(uint[5] memory nums ) external pure returns(uint){
+        uint total = 0;
+        for(uint i = 0; i < nums.length; i++){
+            total += nums[i];
+        }
+        return total;
+    }
+}
