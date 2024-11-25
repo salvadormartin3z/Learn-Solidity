deaef6fb4aee27ed3ecc54946c4cee5c6fdaad32
salvador <salvador.martinez.ksmg@gmail.com>
2024-11-25 17:16:41 -0600

    Dynamic Sum

--- 3 Reference Types/1-Arrays.sol
+++ 3 Reference Types/1-Arrays.sol
@@ -18,3 +18,21 @@ Find the sum of the unsigned integers and return it as a uint.
         return total;
     }
 }
+
+/* 
+2: Dynamic Sum
+
+Your Goal: Sum Dynamic Array
+Create a pure, external function sum which takes a dynamic size array of unsigned integers.
+Find the sum of the unsigned integers and return it as a uint.
+ */
+
+ contract Contract2 {
+    function sum(uint[] memory nums) external pure returns(uint){
+        uint total = 0;
+        for(uint i = 0; i < nums.length; i++){
+            total += nums[i];
+        }
+        return total;
+    }
+}
