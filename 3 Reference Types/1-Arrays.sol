7783319a3893969cc4796976ebb3efa7443eccfd
salvador <salvador.martinez.ksmg@gmail.com>
2024-11-25 17:28:37 -0600

    Filter to Storage

--- 3 Reference Types/1-Arrays.sol
+++ 3 Reference Types/1-Arrays.sol
@@ -36,3 +36,23 @@ Find the sum of the unsigned integers and return it as a uint.
         return total;
     }
 }
+
+/* 
+3: Filter to Storage
+
+Your Goal: Filter Even Numbers
+Create a public, dynamic sized array of unsigned integers as a state variable called evenNumbers.
+Create an external function called filterEven which takes an dynamic size array of unsigned integers as its only argument. Find all of the even numbers in this array and push them into the evenNumbers storage array.
+ */
+
+contract Contract3 {
+    uint[] public evenNumbers;
+
+    function filterEven(uint[] memory nums) external{
+        for(uint i = 0; i < nums.length; i++){
+            if((nums[i]%2) == 0){
+                evenNumbers.push(nums[i]);
+            }
+        }
+    }
+}
