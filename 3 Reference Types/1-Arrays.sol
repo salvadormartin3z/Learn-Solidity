2e88386621dcb70488f750ba39c27f658d2dce0e
salvador <salvador.martinez.ksmg@gmail.com>
2024-11-26 10:10:15 -0600

    Filter to Memory

--- 3 Reference Types/1-Arrays.sol
+++ 3 Reference Types/1-Arrays.sol
@@ -56,3 +56,37 @@ contract Contract3 {
         }
     }
 }
+
+/* 
+4: Filter to Memory
+
+Your Goal: Filter Even Numbers
+Create a pure, external function called filterEven which takes an dynamic size array of unsigned integers as its only argument.
+Find all the even numbers and add them to a new array in memory. This array should contain only the even numbers.
+Return this new array.
+ Note: the return array should only be large enough to fit the even numbers inside. This means you'll need to figure out how many even numbers there are before instantiating the array.
+ */
+
+contract Contract4 {
+    function filterEven(uint[] memory nums) external pure returns(uint[] memory){
+        uint count = 0;
+        for(uint i = 0; i < nums.length; i++){
+            if((nums[i]%2)==0){
+                count++;
+            }
+        }
+
+        uint[] memory evenNumbers = new uint[](count);
+        uint index = 0;
+
+        for(uint i = 0; i < nums.length; i++){
+            if((nums[i]%2)==0){
+                evenNumbers[index] = nums[i];
+                index++;
+            }
+        }
+
+        return evenNumbers;
+    }
+}
+
