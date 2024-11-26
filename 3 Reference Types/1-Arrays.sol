c09775915fa28b4777cc696f8c923b225bae0c64
salvador <salvador.martinez.ksmg@gmail.com>
2024-11-26 10:52:31 -0600

    Stack Club 2

--- 3 Reference Types/1-Arrays.sol
+++ 3 Reference Types/1-Arrays.sol
@@ -9,10 +9,10 @@ Create a pure, external function sum which takes a fixed size array of five unsi
 Find the sum of the unsigned integers and return it as a uint.
  */
 
- contract Contract {
-    function sum(uint[5] memory nums ) external pure returns(uint){
+contract Contract {
+    function sum(uint[5] memory nums) external pure returns (uint) {
         uint total = 0;
-        for(uint i = 0; i < nums.length; i++){
+        for (uint i = 0; i < nums.length; i++) {
             total += nums[i];
         }
         return total;
@@ -27,10 +27,10 @@ Create a pure, external function sum which takes a dynamic size array of unsigne
 Find the sum of the unsigned integers and return it as a uint.
  */
 
- contract Contract2 {
-    function sum(uint[] memory nums) external pure returns(uint){
+contract Contract2 {
+    function sum(uint[] memory nums) external pure returns (uint) {
         uint total = 0;
-        for(uint i = 0; i < nums.length; i++){
+        for (uint i = 0; i < nums.length; i++) {
             total += nums[i];
         }
         return total;
@@ -48,9 +48,9 @@ Create an external function called filterEven which takes an dynamic size array 
 contract Contract3 {
     uint[] public evenNumbers;
 
-    function filterEven(uint[] memory nums) external{
-        for(uint i = 0; i < nums.length; i++){
-            if((nums[i]%2) == 0){
+    function filterEven(uint[] memory nums) external {
+        for (uint i = 0; i < nums.length; i++) {
+            if ((nums[i] % 2) == 0) {
                 evenNumbers.push(nums[i]);
             }
         }
@@ -68,10 +68,12 @@ Return this new array.
  */
 
 contract Contract4 {
-    function filterEven(uint[] memory nums) external pure returns(uint[] memory){
+    function filterEven(
+        uint[] memory nums
+    ) external pure returns (uint[] memory) {
         uint count = 0;
-        for(uint i = 0; i < nums.length; i++){
-            if((nums[i]%2)==0){
+        for (uint i = 0; i < nums.length; i++) {
+            if ((nums[i] % 2) == 0) {
                 count++;
             }
         }
@@ -79,8 +81,8 @@ contract Contract4 {
         uint[] memory evenNumbers = new uint[](count);
         uint index = 0;
 
-        for(uint i = 0; i < nums.length; i++){
-            if((nums[i]%2)==0){
+        for (uint i = 0; i < nums.length; i++) {
+            if ((nums[i] % 2) == 0) {
                 evenNumbers[index] = nums[i];
                 index++;
             }
@@ -106,13 +108,65 @@ contract StackClub {
         members.push(newAddress);
     }
 
-    function isMember(address comapareAddress) public view returns(bool){
-        for(uint i = 0; i < members.length; i++){
-            if(members[i]==comapareAddress){
+    function isMember(address comapareAddress) public view returns (bool) {
+        for (uint i = 0; i < members.length; i++) {
+            if (members[i] == comapareAddress) {
                 return true;
                 break;
             }
         }
         return false;
+    }
+}
+
+/* 
+6: Stack Club 2
+
+Your Goal: Remove Members
+Create a constructor which will add the deployer address as the first member of the stack club.
+Create a removeLastMember function which will remove the last member added to the club.
+ Function Security
+Ensure that the removeLastMember function can only be called by an existing member
+Ensure that addMember can only be called by an existing member
+ */
+
+contract StackClub2 {
+    address[] members;
+
+    constructor() {
+        members.push(msg.sender);
+    }
+
+    function addMember(address newAddress) external onlyMembers {
+        members.push(newAddress);
+    }
+
+    function isMember(address comapareAddress) public view returns (bool) {
+        for (uint i = 0; i < members.length; i++) {
+            if (members[i] == comapareAddress) {
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+
+    function removeLastMember() external onlyMembers {
+        members.pop();
+    }
+
+    modifier onlyMembers() {
+        bool isAllowed = false;
+        for (uint i = 0; i < members.length; i++) {
+            if (msg.sender == members[i]) {
+                isAllowed = true;
+                break;
+            }
+        }
+        if (isAllowed) {
+            _;
+        } else {
+            revert("You are not allowed");
+        }
     }
 }
